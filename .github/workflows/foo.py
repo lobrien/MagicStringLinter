a162ab57061cc53695ec875b5b9649cd9627f1be
lobrien <lobrien@knowing.net>
2019-10-24 08:18:35 -1000

    a

--- .github/workflows/foo.py
+++ .github/workflows/foo.py
@@ -13,8 +13,11 @@ for f in os.listdir("."):
 path = os.environ['GITHUB_EVENT_PATH']
 print('Event path is %s' % path)
 event = readJson(path)
+print(json.dumps(event['head_commit'], indent=2))
 url = event['head_commit']['url']
 print('HEAD is at %s' % url)
+sha = event['head_commit']['sha']
+print(f"SHA is {sha}")
 url = 'https://api.github.com/repos/lobrien/MagicStringLinter/commits/1445bb6f501df462f2480eba3bf924782198a05a'
 print('HARDCODE is at %s' % url)
 tree_response = requests.get(url)
