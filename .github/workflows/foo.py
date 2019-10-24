1445bb6f501df462f2480eba3bf924782198a05a
lobrien <lobrien@knowing.net>
2019-10-24 07:44:59 -1000

    a

--- .github/workflows/foo.py
+++ .github/workflows/foo.py
@@ -6,7 +6,7 @@ def readJson(path) :
     with open(path) as json_file:
         data = json.load(json_file)
         return data
-        
+
 print(os.environ['PATH'])
 for f in os.listdir("."):
     print(f)
@@ -17,7 +17,7 @@ url = event['head_commit']['url']
 print('HEAD is at %s' % url)
 url = 'https://api.github.com/repos/lobrien/MagicStringLinter/git/tree/b6bece5aeaecf7390bddc54de03178393c6390ab'
 print('HARDCODE is at %s' % url)
-tree_response = requests.get(ur)
+tree_response = requests.get(url)
 tree_json = json.loads(tree_response.content)
 print(json.dumps(tree_json, indent=2))
 
